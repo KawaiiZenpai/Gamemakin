3820ec78fd90bef348c1df69056c0b8b5858a53b
Brylan <Shadowlugia0@gmail.com>
2018-10-03 00:39:03 -0500

    This is the true final commit for project 1

--- Project1/project1/project1/Form1.cs
+++ Project1/project1/project1/Form1.cs
@@ -12,17 +12,34 @@ namespace project1
 {
     public partial class Form1 : Form
     {
+        private static int frame= 0;
+        public static int Frame
+        {
+            get { return frame; }
+        }
         public Form1()
         {
             InitializeComponent();
         }
+        
         protected override void OnPaint(PaintEventArgs e)
         {
             int w = this.ClientSize.Width;
             int h = this.ClientSize.Height;
             e.Graphics.FillRectangle(Brushes.White, 0, 0, w, h);
             Sprites.Draws(e);
-            
+            Sprites.AnimeD(e);
+
+            System.Windows.Forms.Timer t = new System.Windows.Forms.Timer();
+            t.Interval = 16;
+            t.Tick += new EventHandler(timer_Tick);
+            t.Start();
+
+            void timer_Tick(object sender, EventArgs p)
+            {
+                frame++;
+                Refresh();
+            }
         }
 
         private void Form1_MouseClick(object sender, MouseEventArgs f)
@@ -33,7 +50,7 @@ namespace project1
             int j = f.Y;
             Sprites.Scans(w, h, i, j);
             
-            Refresh();    
+                
         }
     }
 }
--- Project1/project1/project1/Sprites.cs
+++ Project1/project1/project1/Sprites.cs
@@ -7,7 +7,7 @@ using System.Drawing;
 using System.Drawing.Drawing2D;
 using System.Data;
 using System.Windows.Forms;
-
+//USe %
 namespace project1
 {
     class Sprites
@@ -16,6 +16,12 @@ namespace project1
         public static int page = 0;
         public static int waifhp = 100;
         public static int skelehp = 10;
+        public static int disp = 0;
+        public static int skelef = 0;
+        public static bool hit = false;
+        public static int hj = 0;
+        public static int hi = 0;
+        public static int c = 0;
         public static void Draws(PaintEventArgs e)
         {
             FontFamily fontFamily = new FontFamily("Arial");
@@ -28,7 +34,8 @@ namespace project1
             String w = ("Dead!");
             Image Skeleton = Image.FromFile("Skeleton.jpg");
             Image Waif = Image.FromFile("002.png");
-            Image newImage = Image.FromFile("arrow.jpg");
+            Image Arrow = Image.FromFile("arrow.jpg");
+            
             if (skelehp <= 0 && waifhp <= 0)
             {
                 String win = ("Winner!");
@@ -38,8 +45,8 @@ namespace project1
             {
                 if (page == 0)
                 {
-                    e.Graphics.DrawImage(newImage, 250, 250, 100, 100);
-                    e.Graphics.DrawImage(Waif, 0, 0, 250, 250);
+                    e.Graphics.DrawImage(Arrow, 250, 250, 100, 100);
+                    e.Graphics.DrawImage(Waif, 0+disp, 0+disp, 250, 250);
                     String waifhptxt = ("Hp" + waifhp);
                     if (waifhp > 0)
                     {
@@ -54,8 +61,13 @@ namespace project1
                 }
                 if (page == 1)
                 {
-                    e.Graphics.DrawImage(newImage, 250, 250, 100, 100);
-                    e.Graphics.DrawImage(Skeleton, 0, 0, 250, 250);
+                    e.Graphics.DrawImage(Arrow, 250, 250, 100, 100);
+                    int y = 0;
+                    if (Form1.Frame % 5 == 0&&y==0) y++;
+                    else if (Form1.Frame % 5 == 0 && y == 1) y--;
+                    if (y == 1) e.Graphics.DrawImage(Skeleton, 0, 0, 250, 250);
+                    if (y == 0) e.Graphics.DrawImage(Skeleton, 250, 0, -250, 250);
+
                     String skelehptxt = ("Hp" + skelehp);
                     if (skelehp > 0)
                     {
@@ -69,14 +81,43 @@ namespace project1
                 }
             }
         }
+        public static void AnimeD(PaintEventArgs e)
+        {
+            
+            if (hit == true)
+            {
+                
+                Image kirbyA = Image.FromFile("kirbysprites.png");
+                int x = (kirbyA.Width / 5)+3;
+                int y = kirbyA.Height;
+                Rectangle sourceRect = new Rectangle(x * c, 0, x, y);
+                if (Form1.Frame % 5 == 0 && c < 5) c++;
+                else if (Form1.Frame % 5 == 0 && c == 5)
+                {
+                    c = 0;
+                    hit = false;
+                }
+                e.Graphics.DrawImage(kirbyA,hi-(y/2),hj-(x/2), sourceRect, GraphicsUnit.Pixel);
+                Console.WriteLine(c);
+
+        
+                
+
+            }
+            
+
+
+        }
         public static void Scans(int w, int h,int i, int j)
         {
+            hj = j;
+            hi = i;
             if(page == 0)
             {
                 if ((0 < i && i < 250) && (0 < j && j < 250))
                 {
                     waifhp -= 1;
-
+                    hit = true;
                 }
 
                 if ((250 < i && i < 350) && (250 < j && j < 350))
@@ -90,7 +131,8 @@ namespace project1
                 if ((0 < i && i < 250) && (0 < j && j < 250))
                 {
                     skelehp -= 1;
-
+                    hit = true;
+                    
                 }
                 if ((250 < i && i < 350) && (250 < j && j < 350))
                 {
